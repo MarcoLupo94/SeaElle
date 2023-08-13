418fd7795530dc082436cb143196942f4f7295ac
marco <marco.lupo@missionlabs.co.uk>
2023-08-13 11:22:44 +0100

    add hamburger menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,28 +1,25 @@
-import { HamburgerIcon } from '@chakra-ui/icons'
 import {
     Box,
     Link as ChakraLink,
     ChakraProps,
-    Drawer,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerHeader,
-    DrawerOverlay,
+    Divider,
     Flex,
-    IconButton,
-    Text
+    Text,
+    useDisclosure
 } from '@chakra-ui/react'
 import { FC, useEffect, useState } from 'react'
-
+import mobile from 'is-mobile'
+import Hamburger from 'hamburger-react'
 interface NavBarProps {}
 
 export const NavBar: FC<NavBarProps> = () => {
-    const [isMenuOpen, setMenuOpen] = useState(false)
     const [isScrolled, setScrolled] = useState(false)
+    const [isMobile] = useState(mobile())
 
-    const handleMenuToggle = () => {
-        setMenuOpen(!isMenuOpen)
+    const { isOpen, onToggle } = useDisclosure()
+
+    const handleToggleMenu = () => {
+        onToggle()
     }
 
     useEffect(() => {
@@ -38,94 +35,91 @@ export const NavBar: FC<NavBarProps> = () => {
     }, [])
 
     return (
-        <Flex
-            as="nav"
-            justifyContent="space-between"
-            alignItems="center"
-            px={4}
-            h="60px" // You can adjust the height as needed
-            bg={isScrolled ? 'brand.100' : 'transparent'} // Change background color when scrolled
-            color={isScrolled ? 'white' : 'black'} // Change text color when scrolled
-            boxShadow={isScrolled ? 'md' : 'none'} // Add shadow effect when scrolled
-            position="fixed"
-            top={0}
-            left={0}
-            right={0}
-            zIndex="45"
-            style={{
-                transition: 'background-color 0.3s, color 0.3s, box-shadow 0.3s' // Add smooth transitions
-            }}
-        >
-            {/* Custom Logo */}
-            <Text style={{ fontSize: '2rem' }} fontWeight="bold" fontFamily="Dancing Script">
-                SeaElle
-            </Text>
-
-            {/* Navigation Links */}
+        <>
+            {' '}
             <Flex
-                display={{ base: 'none', md: 'flex' }} // Hide on mobile, show on medium and larger screens
-                align="center"
+                as="nav"
+                justifyContent="space-between"
+                alignItems="center"
+                px={4}
+                h="60px" // You can adjust the height as needed
+                bg={isScrolled || isMobile ? 'brand.100' : 'transparent'} // Change background color when scrolled
+                color={isScrolled || isMobile ? 'white' : 'black'} // Change text color when scrolled
+                boxShadow={isScrolled ? 'md' : 'none'} // Add shadow effect when scrolled
+                position={['absolute', 'fixed']}
+                top={0}
+                left={0}
+                right={0}
+                zIndex={400}
+                style={{
+                    transition: 'background-color 0.3s, color 0.3s, box-shadow 0.3s' // Add smooth transitions
+                }}
             >
-                <NavLink path="services" text="Services" />
-                <NavLink path="about" text="About Me" />
-                <NavLink path="portfolio" text="Portfolio" />
-                <NavLink path="testimonials" text="Testimonials" />
-                <NavLink path="contact-me" text="Contact Me" />
-            </Flex>
-            {/* Socials Links */}
-            <Flex
-                display={{ base: 'none', md: 'flex' }} // Hide on mobile, show on medium and larger screens
-                align="center"
-            >
-                <NavLink path="services" text="Baking School" />
-                {/* <NavLink path="about" text="Instagram" /> */}
-                {/* <NavLink path="portfolio" text="Linkedin" /> */}
-            </Flex>
+                {/* Custom Logo */}
+                <Text style={{ fontSize: '2rem' }} fontWeight="bold" fontFamily="Dancing Script">
+                    SeaElle
+                </Text>
 
+                {/* Navigation Links */}
+                <Flex
+                    display={{ base: 'none', md: 'flex' }} // Hide on mobile, show on medium and larger screens
+                    align="center"
+                >
+                    <NavLink path="services" text="Services" />
+                    <NavLink path="about" text="About Me" />
+                    <NavLink path="portfolio" text="Portfolio" />
+                    <NavLink path="testimonials" text="Testimonials" />
+                    <NavLink path="contact-me" text="Contact Me" />
+                </Flex>
+                {/* Socials Links */}
+                <Flex
+                    display={{ base: 'none', md: 'flex' }} // Hide on mobile, show on medium and larger screens
+                    align="center"
+                >
+                    <NavLink path="services" text="Baking School" />
+                    {/* <NavLink path="about" text="Instagram" /> */}
+                    {/* <NavLink path="portfolio" text="Linkedin" /> */}
+                </Flex>
+                {isMobile && (
+                    <Hamburger
+                        toggled={isOpen}
+                        color={isOpen ? '#`D75559' : '#FFFFFF'}
+                        toggle={handleToggleMenu}
+                    />
+                )}
+            </Flex>
             {/* Hamburger Menu for Mobile */}
-            <Box display={{ base: 'block', md: 'none' }}>
-                <IconButton
-                    aria-label="Menu"
-                    icon={<HamburgerIcon />}
-                    onClick={handleMenuToggle}
-                    color={isScrolled ? 'white' : 'black'} // Change icon color when scrolled
-                    variant="outline"
-                    _hover={{ color: 'brand.500' }}
-                />
-                <Drawer placement="right" onClose={() => setMenuOpen(false)} isOpen={isMenuOpen}>
-                    <DrawerOverlay />
-                    <DrawerContent bg={'brand.500'}>
-                        <DrawerCloseButton />
-                        <DrawerHeader>Menu</DrawerHeader>
-                        <DrawerBody>
-                            <Box as="nav">
-                                <NavLink
-                                    path="services"
-                                    text="Services"
-                                    onClick={handleMenuToggle}
-                                />
-                                <NavLink path="about" text="About Me" onClick={handleMenuToggle} />
-                                <NavLink
-                                    path="portfolio"
-                                    text="Portfolio"
-                                    onClick={handleMenuToggle}
-                                />
-                                <NavLink
-                                    path="testimonials"
-                                    text="Testimonials"
-                                    onClick={handleMenuToggle}
-                                />
-                                <NavLink
-                                    path="contact-me"
-                                    text="Contact Me"
-                                    onClick={handleMenuToggle}
-                                />
-                            </Box>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
-            </Box>
-        </Flex>
+            <Flex
+                direction="column"
+                position="absolute"
+                alignItems={'center'}
+                left={0}
+                top={10}
+                w="100%"
+                h="50vh"
+                bg="brand.100"
+                boxShadow="0px 2px 3px 0px rgba(0, 0, 0, 0.04)"
+                zIndex={100}
+                transition="transform 0.5s ease-in-out"
+                transform={isOpen ? 'translateY(0)' : 'translateY(-100%)'}
+                opacity={isOpen ? 1 : 0}
+                gap={4}
+                pt={6}
+                onClick={handleToggleMenu}
+            >
+                <Divider opacity={0.1} />
+                <NavLink fontSize={'2xl'} path="portfolio" text="portfolio" />
+                <Divider opacity={0.1} />
+                <NavLink fontSize={'2xl'} path="services" text="services" />
+                <Divider opacity={0.1} />
+                <NavLink fontSize={'2xl'} path="about" text="about" />
+                <Divider opacity={0.1} />
+                <NavLink fontSize={'2xl'} path="contact-me" text="contact" />
+                <Divider opacity={0.1} />
+                <NavLink fontSize={'2xl'} path="testimonials" text="testimonials" />
+                <Divider opacity={0.1} />
+            </Flex>
+        </>
     )
 }
 
